8ce6cd841ce2ad689bc5e4f02a8830f46fdecbf2
mabo207 <mabo168general+github@gmail.com>
2018-06-02 18:04:45 +0900

    キーボード入力を受け付けて速度を調整できるようになった

--- JSGame/src/main.js
+++ JSGame/src/main.js
@@ -1,18 +1,56 @@
-var Trocco=function(x,y){
-	//位置
-	this.x=x;
-	this.y=y;
-	//速度
-	this.vx=2;
-	this.vy=0;
-	//角度
-	this.angle=0;//横向き。時計回りが正
+const Trocco=class{
+	constructor(x,y){
+		//位置
+		this.x=x;
+		this.y=y;
+		//速度
+		this.vx=0;
+		this.vy=0;
+		//角度
+		this.angle=0;//横向き。時計回りが正
+	};
 };
 
+const KeyInput=class{
+	constructor(){
+		this.acceed=false;
+		this.decelerate=false;
+		this.jump=false;
+	};
+};
 
 var canvas;
 var context;
+var keyinput;
 var player;
+
+const KeyPress=(e)=>{
+	switch(e.keyCode){
+	case 90://z
+		keyinput.decelerate=true;
+		break;
+	case 88://x
+		keyinput.jump=true;
+		break;
+	case 67://c
+		keyinput.acceed=true;
+		break;
+	}
+};
+KeyRelease=(e)=>{
+	switch(e.keyCode){
+	case 90://z
+		keyinput.decelerate=false;
+		break;
+	case 88://x
+		keyinput.jump=false;
+		break;
+	case 67://c
+		keyinput.acceed=false;
+		break;
+	}
+}
+
 
 const ProcessGameloop=()=>{
 	//描画
@@ -30,7 +68,19 @@ const ProcessGameloop=()=>{
 	context.fill();
 	context.closePath();
 	//更新
-	
+	console.log(keyinput.jump);
+	if(keyinput.acceed==true){
+		//Cを押していた時は加速
+		player.vx++;
+	}
+	if(keyinput.decelerate==true){
+		//Zを押していた時は減速
+		player.vx--;
+	}
+	if(keyinput.jump==true){
+		//Xを押すとジャンプ
+		player.vy+=-10;
+	}
 	player.x+=player.vx;
 	player.y+=player.vy;
 	
@@ -39,8 +89,14 @@ const ProcessGameloop=()=>{
 const main=()=>{
 	canvas=document.getElementById("canvas");
 	context=canvas.getContext("2d");
+	keyinput=new KeyInput();
 	//トロッコを用意する
 	player=new Trocco(20,400);
+	//キーボード更新ハンドラを動かす
+	//document.addEventListener("keydown",keyinput.KeyPress);//これだとthisがdocumentを指してしまうのでだめ
+	//document.addEventListener("keyup",keyinput.KeyRelease);
+	document.addEventListener("keydown",KeyPress);
+	document.addEventListener("keyup",KeyRelease);
 	
 	//ゲーム部分
 	setInterval(ProcessGameloop,33);
